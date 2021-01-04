6976f12a774449ae354e730d985a515842425025
Rambh71 <73946122+Rambh71@users.noreply.github.com>
2021-01-04 10:43:02 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,21 @@
+var dog, happyDog, database, foodS, foodStock;
+function preload()
+{
+	//load images here
+}
+
+function setup() {
+	createCanvas(500,500);
+  
+}
+
+
+function draw() {  
+
+  drawSprites();
+  //add styles here
+
+}
+
+
+
